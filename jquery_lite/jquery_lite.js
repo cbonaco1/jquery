57f9053239e4b023106f956c4cb85735185b59c2
Christian and Daniel <appacademy@Apps-Mac-mini-51.local>
2016-01-14 13:58:35 -0500

    Start JQuery, finish append.

---
+++ jquery_lite/jquery_lite.js
@@ -0,0 +1,55 @@
+(function() {
+  //makes this global
+  //put left side as shorthand instead of window.$l
+  var $l = window.$l = function(arg) {
+    if (arg instanceof HTMLElement) {
+      return new DOMNodeCollection([arg]);
+    } else {
+      var elementList = document.querySelectorAll(arg);
+      elementList = Array.prototype.slice.call(elementList);
+      return new DOMNodeCollection(elementList);
+    }
+  };
+
+  var DOMNodeCollection = function (htmlElements) {
+      this.htmlElements = htmlElements;
+  };
+
+  DOMNodeCollection.prototype.html = function(str){
+    if (typeof(str) === "undefined") {
+      if (typeof this.htmlElements === "undefined"){
+        return null;
+      } else {
+        return this.htmlElements[0].innerHTML;
+      }
+    }
+    else {
+      this.htmlElements.forEach(function(element){
+        element.innerHTML = str;
+      });
+    }
+  };
+
+  DOMNodeCollection.prototype.empty = function(){
+    this.html("");
+  };
+
+  DOMNodeCollection.prototype.append = function(arg){
+    if (arg instanceof DOMNodeCollection) {
+      arg.htmlElements.forEach(this.append.bind(this));
+    } else if (typeof(arg) === "string") {
+      this.htmlElements.forEach(function(el){
+        el.innerHTML += arg;
+      });
+    } else if (arg instanceof HTMLElement) {
+      if (this.htmlElements.length > 0) {
+        this.htmlElements[0].appendChild(arg);
+
+        for (var i = 1; i < this.htmlElements.length; i++) {
+          this.htmlElements[i].innerHTML += arg.outerHTML;
+        }
+      }
+    }
+  };
+
+})();
